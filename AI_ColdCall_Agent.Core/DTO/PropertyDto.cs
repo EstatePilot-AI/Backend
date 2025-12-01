c963602de503d5a098a4e721e71e4921c1b4dea5
yussuftoba <yussuftoba@gmail.com>
2025-12-01 07:09:16 +0200

    Add empty DTO, Interfaces, IServices, Services and Repositories folders

---
+++ AI_ColdCall_Agent.Core/DTO/PropertyDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO;
+
+public class PropertyDto
+{
+}
---
+++ AI_ColdCall_Agent.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,10 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories;
+
+public class Repository<T>: IRepository<T> where T : class
+{
+}
