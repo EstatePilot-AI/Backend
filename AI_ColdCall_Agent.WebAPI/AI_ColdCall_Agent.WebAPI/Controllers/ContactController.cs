cb0ae05d518809c76e6479c8266592d294a86b0c
yussuftoba <yussuftoba@gmail.com>
2025-12-08 15:11:41 +0200

    add contact controller

---
+++ AI_ColdCall_Agent.WebAPI/AI_ColdCall_Agent.WebAPI/Controllers/ContactController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AI_ColdCall_Agent.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ContactController : ControllerBase
+	{
+	}
+}
