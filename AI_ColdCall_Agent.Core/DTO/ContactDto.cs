3a3afd179b641c399357fa6e6570bb7cbee5e02c
yussuftoba <yussuftoba@gmail.com>
2025-12-09 22:17:19 +0200

    add buyer contact and create lead request

---
+++ AI_ColdCall_Agent.Core/DTO/ContactDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DTO;
+
+public class ContactDto
+{
+	[Required, MaxLength(100)]
+	public string Name { get; set; }
+
+	[Required]
+	[RegularExpression(@"^(\+20)?(010|011|012|015)[0-9]{8}$", ErrorMessage = "Phone number must be a valid Egyptian mobile number")]
+	public string Phone { get; set; }
+
+	[Required]
+	[EmailAddress(ErrorMessage = "Email should be in a proper email address format")]
+	public string Email { get; set; }
+}
--- AI_ColdCall_Agent.WebAPI/AI_ColdCall_Agent.WebAPI/Controllers/ContactController.cs
+++ AI_ColdCall_Agent.WebAPI/AI_ColdCall_Agent.WebAPI/Controllers/ContactController.cs
@@ -1,11 +1,73 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CsvHelper;
+using DTO;
+using Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Models;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 
-namespace AI_ColdCall_Agent.WebAPI.Controllers
+namespace Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ContactController : ControllerBase
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class ContactController : ControllerBase
+	private readonly IUnitOfWork _unitOfWork;
+
+	public ContactController(IUnitOfWork unitOfWork)
 	{
+		_unitOfWork = unitOfWork;
 	}
+
+	//Add and new contact to a buyer and add leadRequest by using buyerId and propertyId comes from the shop
+	[HttpPost("AddBuyerContact{id:int}")]
+	public async Task<IActionResult> AddBuyerContact(ContactDto contactDto, int id)
+	{
+		if (ModelState.IsValid)
+		{
+			if (id <= 0)
+			{
+				return BadRequest("Invalid buyer ID.");
+			}
+			var property = await _unitOfWork.Properties.GetByIdAsync(id);
+
+			if (property == null) // check if property exists
+			{
+				return NotFound("Property not found.");
+			}
+			var contact = new Contact()
+			{
+				Name = contactDto.Name,
+				Phone = contactDto.Phone,
+				Email = contactDto.Email,
+				ContactTypeId = 1,  //1 for buyer
+				ContactStatusId = 1 //1 for pending_call
+			};
+
+			await _unitOfWork.Contacts.AddAsync(contact); //store contact for buyer
+			_unitOfWork.Save();
+
+			var leadRequest = new LeadRequest()
+			{
+				BuyerContactId = contact.ContactId,
+				PropertyId = id,
+				LeadRequestStatusId = 1 //1 for Pending Call
+			};
+
+			await _unitOfWork.LeadRequests.AddAsync(leadRequest); //store leadRequest
+			_unitOfWork.Save();
+
+			return Ok(new
+			{
+				Message = "Contact and Lead Request added successfully",
+				BuyerContactId = contact.ContactId,
+				LeadRequestId = leadRequest.RequestId
+			});
+		}
+		return BadRequest(ModelState);
+	}
+
+
+	
 }
