0a6a6aee796b3f33e124b4ffb272ced441c8557e
yussuftoba <yussuftoba@gmail.com>
2026-01-31 21:52:07 +0200

    delete createproperty

--- AI_ColdCall_Agent.WebAPI/AI_ColdCall_Agent.WebAPI/Program.cs
+++ AI_ColdCall_Agent.WebAPI/AI_ColdCall_Agent.WebAPI/Program.cs
@@ -112,6 +112,15 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+//if (!app.Environment.IsDevelopment())
+//{
+//	app.UseSwagger();
+//	app.UseSwaggerUI(c =>
+//	{
+//		c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+//		c.RoutePrefix = string.Empty; // Set to empty string to serve at root
+//	});
+//}
 
 app.UseHttpsRedirection();
 
