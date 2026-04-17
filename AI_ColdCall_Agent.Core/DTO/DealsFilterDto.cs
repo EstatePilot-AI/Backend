cc1767239098e324b32483e71059fe4abafd0678
yussuftoba <yussuftoba@gmail.com>
2026-04-18 00:55:19 +0200

    add pagination to getAllDeals

---
+++ AI_ColdCall_Agent.Core/DTO/DealsFilterDto.cs
@@ -0,0 +1,34 @@
+﻿using AI_ColdCall_Agent.Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO;
+
+public class DealsFilterDto
+{
+	private int _pageNumber = 1;
+	private int _pageSize = 10;
+
+	public int PageNumber
+	{
+		get => _pageNumber;
+		set => _pageNumber = value < 1 ? 1 : value;
+	}
+
+	public int PageSize
+	{
+		get => _pageSize;
+		set => _pageSize = value < 1 ? 10 : (value > 50 ? 50 : value);
+	}
+
+	public DealStatus? DealStatusId { get; set; }
+}
+
+public enum DealStatus
+{
+	InProgress = 1,
+	Completed = 2,
+	Canceled = 3
+}
+
